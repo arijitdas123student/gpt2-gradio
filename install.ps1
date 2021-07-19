8d05aefb58952d5f022b49d67a7c41638c48ee54
Arijit Das <arijitdas18022006@gmail.com>
2021-07-19 11:56:47 +0530

    change choco install procedure as per official ins

--- install.ps1
+++ install.ps1
@@ -1,5 +1,4 @@
-$script.DownloadString("https://chocolatey.org/install.ps1")
-Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco upgrade chocolatey -y
 choco install git -y
 choco install python3 -y
